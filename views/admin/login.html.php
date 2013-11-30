f823fb29d8566ef4fe4d4b72d7b77d0c158a99dd
Juan Puelpan <jpuelpan528@gmail.com>
2013-11-30 01:31:55 -0300

    Remove error alert from login view

--- views/admin/login.html.php
+++ views/admin/login.html.php
@@ -1,8 +1,5 @@
 <form action="/admin/login" method="POST">
   <h2>Iniciar sesión</h2>
-  <?php if( flash('error') ){ ?>
-    <p class="alert alert-danger"><?php echo flash('error') ?></p>
-  <?php } ?>
   <div class="form-group">
     <label for="exampleInputEmail1">Correo electrónico</label>
     <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
